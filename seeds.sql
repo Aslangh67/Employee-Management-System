8f7a25fa98b7e9a1aff34783c2f8ccd9686bef6c
Mr. A <ag.perch@gmail.com>
2020-01-10 15:47:34 -0800

    Adding new files, server and sql

---
+++ seeds.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS E_M_S;
+CREATE DATABASE E_M_S;
+
+USE E_M_S;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT(10) NOT NULL,
+  manager_id INT(10),
+  PRIMARY KEY (id)
+);
+
