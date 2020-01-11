db4ed5e2858565ddd39658a0ad6ca6383b9a4490
Mr. A <ag.perch@gmail.com>
2020-01-10 18:38:41 -0800

    server and sql are running

--- seeds.sql
+++ seeds.sql
@@ -18,7 +18,7 @@ CREATE TABLE role(
 );
 
 
-CREATE TABLE department(
+CREATE TABLE employee(
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
--- server.js
+++ server.js
@@ -1,24 +1,118 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-// create the connection information for the sql database
+
 var connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "password",
-  database: "greatBay_DB"
+  database: "E_M_S"
 });
 
-// connect to the mysql server and sql database
 connection.connect(function(err) {
   if (err) throw err;
-  // run the start function after the connection is made to prompt the user
   start();
 });
+
+function start() {
+    inquirer
+      .prompt({
+        name: "opt",
+        type: "list",
+        message: "Please chose an option?",
+        choices: ["ADD a department, employee, or a role", "VIEW a department, employee, or a role", "UPDATE employee role"]
+      })
+      .then(function(answer) {
+        if (answer.opt === "ADD a department, employee, or a role") {
+          add();
+        }
+        else if(answer.opt === "VIEW a department, employee, or a role") {
+        //   view();
+        } else{
+          connection.end();
+        }
+      });
+  }
+  
+
+  function add() {
+    inquirer
+      .prompt({
+        name: "adding",
+        type: "list",
+        message: "Please chose an option?",
+        choices: ["ADD a department", "ADD an employee", "ADD a role"]
+      })
+      .then(function(answer) {
+
+        if (answer.adding === "ADD a department") {
+            inquirer
+            .prompt([
+              {
+                name: "department",
+                type: "input",
+                message: "Please Provide a name for the department?"
+              }
+            
+            ]).then(function(answer) {
+                console.log(answer);
+                          connection.query(
+          "INSERT INTO department SET ?",
+          {
+            name: answer.department
+          },
+    
+          function(err) {
+            if (err) throw err;
+            console.log("Your department was created successfully!");
+            // re-prompt the user for if they want to bid or post
+            start();
+          }
+        );
+            }
+
+            )}
+          else if(answer.adding === "ADD an employee") {
+
+            
+          } else{
+            connection.end();
+          }
+        
+
+        // connection.query(
+        //   "INSERT INTO auctions SET ?",
+        //   {
+        //     item_name: answer.item,
+        //     category: answer.category,
+        //     starting_bid: answer.startingBid || 0,
+        //     highest_bid: answer.startingBid || 0
+        //   },
+        //   function(err) {
+        //     if (err) throw err;
+        //     console.log("Your auction was created successfully!");
+        //     // re-prompt the user for if they want to bid or post
+        //     start();
+        //   }
+        // );
+      });
+  }
+          // connection.query(
+        //   "INSERT INTO department SET ?",
+        //   {
+        //     name: answer.item,
+        //     category: answer.category,
+        //     starting_bid: answer.startingBid || 0,
+        //     highest_bid: answer.startingBid || 0
+        //   },
+        //   function(err) {
+        //     if (err) throw err;
+        //     console.log("Your auction was created successfully!");
+        //     // re-prompt the user for if they want to bid or post
+        //     start();
+        //   }
+        // );
